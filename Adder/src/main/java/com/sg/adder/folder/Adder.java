e0177ed43533f581ef9bd773d1f8b0ba4fff2e01
Greg Barnes <gregkbarnes@gmail.com>
2019-10-20 14:32:58 -0700

    added Adder program

---
+++ Adder/src/main/java/com/sg/adder/folder/Adder.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.adder.folder;
+
+/**
+ *
+ * @author GKB
+ */
+public class Adder {
+    public static void main(String[] args){
+        int operand1 = 0;
+        int operand2 = 0;
+        int sum = 0;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        sum = operand1 + operand2;
+        
+        System.out.println("Sum is: " + sum);
+        
+    }
+}
