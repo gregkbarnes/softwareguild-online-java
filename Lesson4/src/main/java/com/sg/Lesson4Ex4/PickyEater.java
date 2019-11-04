3fd30dba94b3a7f59bbc3efdc4a2cbea856cee97
Greg Barnes <gregkbarnes@gmail.com>
2019-11-03 17:46:19 -0800

    reorganize folders

--- Lesson4/src/main/java/com/sg/Lesson4Ex4/PickyEater.java
+++ Lesson4/src/main/java/com/sg/Lesson4Ex4/PickyEater.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.sg.lesson4;
+package com.sg.Lesson4Ex4;
 
 import java.util.Scanner;
 
--- Lesson4ex6/src/main/java/com/sg/Lesson4Ex6/MaybeItLovesMe.java
+++ Lesson4ex6/src/main/java/com/sg/Lesson4Ex6/MaybeItLovesMe.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.sg.lesson6;
+package com.sg.Lesson4Ex6;
 
 import java.util.Random;
 
