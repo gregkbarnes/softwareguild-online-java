1e27ba8d01d44359e79d93d8695b424a74773bdd
Greg Barnes <gregkbarnes@gmail.com>
2019-11-06 08:12:38 -0800

    lesson 6 - methods

---
+++ FunWithMethods/src/main/java/com/sg/funwithmethods/MethodFun.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.funwithmethods;
+
+/**
+ *
+ * @author GKB
+ */
+public class MethodFun {
+    public static void main(String[] args) {
+        doIt();
+    }
+    
+    public static void doIt() {
+        System.out.println("Hello");
+    }
+}
